4b8277189206794d91977fa47c6fa8f6360f990b
sergey petrachkov <petrachkovsergey@gmail.com>
2018-08-19 16:30:52 +0700

    Initial Commit

---
+++ SwiftyVideoExporter/SwiftyVideoExporter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyVideoExporter.h
+//  SwiftyVideoExporter
+//
+//  Created by Sergey Petrachkov on 19/08/2018.
+//  Copyright © 2018 Sergey Petrachkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyVideoExporter.
+FOUNDATION_EXPORT double SwiftyVideoExporterVersionNumber;
+
+//! Project version string for SwiftyVideoExporter.
+FOUNDATION_EXPORT const unsigned char SwiftyVideoExporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyVideoExporter/PublicHeader.h>
+
+
